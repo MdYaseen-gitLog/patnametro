a4bb7b0928f2bb1dcd1ba26ed0d92bbcd8fef99f
Yaseen <69516235+MdYaseen-gitLog@users.noreply.github.com>
2025-05-29 14:14:05 +0530

    pwa files uploaded

---
+++ service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'patnametro-v1'; // increment version to invalidate old cache
+const urlsToCache = [
+  '/pmetro/Final',
+  '/pmetro/Final/index.html',
+  '/pmetro/Final/manifest.json',
+  '/pmetro/Final/data/icons/icon-192.png',
+  '/pmetro/Final/data/icons/icon-512.png'
+];
+
+// Install event - caching static assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event - cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('[ServiceWorker] Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch event - serve cached content or go to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
