b3191ff4799e722f041d704a920840f4044429e0
Yaseen <69516235+MdYaseen-gitLog@users.noreply.github.com>
2025-05-31 16:18:06 +0530

    bug fix for location marker.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v1.5.0'; // 👈 Change this whenever you update
+const APP_VERSION = 'v1.5.1'; // 👈 Change this whenever you update
 const CACHE_NAME = 'patnametro-' + APP_VERSION;
 const urlsToCache = [
   '/patnametro',
