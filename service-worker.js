8bc5fb88c2818b2349518cbda27dedc31b0868e9
Yaseen <69516235+MdYaseen-gitLog@users.noreply.github.com>
2025-06-03 21:35:01 +0530

    version updated.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v1.5.3'; // 👈 Change this whenever you update
+const APP_VERSION = 'v1.5.4'; // 👈 Change this whenever you update
 const CACHE_NAME = 'patnametro-' + APP_VERSION;
 const urlsToCache = [
   '/patnametro',
