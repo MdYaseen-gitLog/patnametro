c12b434690568e479c0f89446077c8e7efc867a2
Yaseen <69516235+MdYaseen-gitLog@users.noreply.github.com>
2025-06-04 17:46:23 +0530

    new light navbar, heading change, marquee removed.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v1.5.5'; // 👈 Change this whenever you update
+const APP_VERSION = 'v1.6.1'; // 👈 Change this whenever you update
 const CACHE_NAME = 'patnametro-' + APP_VERSION;
 const urlsToCache = [
   '/patnametro',
